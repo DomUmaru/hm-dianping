aab708151e5a34dc21133e463987b13375036264
DarkUmr <2360741607@qq.com>
2025-11-13 01:23:46 +0800

    商户缓存模块: 1、常规redis缓存策略 2、雪崩，穿透，击穿讲解 3、互斥锁解决多线程的应用

--- src/main/java/com/hmdp/controller/ShopTypeController.java
+++ src/main/java/com/hmdp/controller/ShopTypeController.java
@@ -1,14 +1,20 @@
 package com.hmdp.controller;
 
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.service.IShopTypeService;
+import com.hmdp.utils.RedisConstants;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -25,10 +31,33 @@ public class ShopTypeController {
     @Resource
     private IShopTypeService typeService;
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
     @GetMapping("list")
     public Result queryTypeList() {
-        List<ShopType> typeList = typeService
-                .query().orderByAsc("sort").list();
+        // 1. 从Redis查询缓存
+        String key = RedisConstants.CACHE_TYPE_LIST_KEY;
+        String typeListJson = stringRedisTemplate.opsForValue().get(key);
+
+        // 2. 判断缓存是否存在
+        if (StrUtil.isNotBlank(typeListJson)) {
+            // 3. 存在，直接返回
+            List<ShopType> typeList = JSONUtil.toList(typeListJson, ShopType.class);
+            return Result.ok(typeList);
+        }
+
+        // 4. 不存在，查询数据库
+        List<ShopType> typeList = typeService.query().orderByAsc("sort").list();
+
+        // 5. 数据库也没有数据
+        if (CollUtil.isEmpty(typeList)) {
+            return Result.ok(Collections.emptyList());
+        }
+
+        // 6. 将数据写入Redis
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(typeList));
+
         return Result.ok(typeList);
     }
 }
